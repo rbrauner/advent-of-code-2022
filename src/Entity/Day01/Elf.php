3b021e5cd19618fa80fe5d269b837620262f29f7
rbrauner <rafalbrauner@gmail.com>
2022-12-04 14:48:55 +0100

    Create command for 1st day

---
+++ src/Entity/Day01/Elf.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity\Day01;
+
+class Elf
+{
+    public function __construct(
+        private int $calories = 0
+    )
+    {
+    }
+
+    public function append(int $calorie): void
+    {
+        $this->calories += $calorie;
+    }
+
+    public function getCalories(): int
+    {
+        return $this->calories;
+    }
+}
